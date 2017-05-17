44fb06235b74eb35b3124b8e3c1dd9afed02a339
Daniel Pollock <danielstapleton@Daniels-MacBook-Pro.local>
2017-05-16 19:08:50 -0700

    week 10 JS

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var animals = ['dog','cat','turtle','mouse','horse']
+
+for (var i = 0; i < animals.length; i++) {
+	//console.log(animals[i] + animals[i].length)
+}
+animals.reverse()
+//document.write(animals)
+
+var numbers = [109, 33, 324, 52114, 1]
+for (var i = 0; i < numbers.length; i++) {
+	//console.log(numbers.sort())
+	//console.log(numbers.sort( function(a,b){ return b-a}))
+}
+var array = ['hi','i','like']
+array.push('video','games')
+array.shift()
+console.log(array)
